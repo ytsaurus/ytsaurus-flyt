78332fb7a603df18277813daef817493b3ea9a9b
DanilSabirov <danil.sabirov179623@gmail.com>
2025-12-22 18:37:06 +0400

    Fix link

--- flink-connector-ytsaurus/src/main/java/tech/ytsaurus/flyt/connectors/ytsaurus/common/constants/YtErrorCodes.java
+++ flink-connector-ytsaurus/src/main/java/tech/ytsaurus/flyt/connectors/ytsaurus/common/constants/YtErrorCodes.java
@@ -1,7 +1,7 @@
 package tech.ytsaurus.flyt.connectors.ytsaurus.common.constants;
 
 /**
- * See <a href="https://github.com/ytsaurus/ytsaurus/blob/312be22244984a87ba14719060ca9e0a8b3638d5/yt/yt/client/tablet_client/public.h#L61">client</a>
+ * See <a href="https://github.com/ytsaurus/ytsaurus/blob/main/yt/yt/client/tablet_client/public.h">client</a>
  */
 public interface YtErrorCodes {
     int NODE_LOCKED_BY_MOUNT_UNMOUNT = 1706;
