825507b8bb5d45a879c7b60064fcb340d16dcb00
Alexey Akhundov <me@theseems.ru>
2026-05-28 14:55:10 +0300

    fix(python): make `__version__` attr get an actual version

--- python/src/ytsaurus_flyt/__init__.py
+++ python/src/ytsaurus_flyt/__init__.py
@@ -25,7 +25,12 @@ __all__ = [
     "make_yt_client",
 ]
 
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version as _pkg_version
+
+try:
+    __version__ = _pkg_version("ytsaurus-flyt")
+except PackageNotFoundError:
+    __version__ = "0.0.0+unknown"
 
 
 def __getattr__(name):
