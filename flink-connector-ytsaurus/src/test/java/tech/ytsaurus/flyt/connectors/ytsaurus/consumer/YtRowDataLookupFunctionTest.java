b63243adb862b1608f9404aeb6c55b84ce078e72
YahorBurakevich <egor.burakevich93@gmail.com>
2026-09-21 14:56:45 +0300

    test: use neutral cluster name

--- flink-connector-ytsaurus/src/test/java/tech/ytsaurus/flyt/connectors/ytsaurus/consumer/YtRowDataLookupFunctionTest.java
+++ flink-connector-ytsaurus/src/test/java/tech/ytsaurus/flyt/connectors/ytsaurus/consumer/YtRowDataLookupFunctionTest.java
@@ -37,7 +37,7 @@ class YtRowDataLookupFunctionTest {
                 credentialsProvider,
                 "[{name=id;type=int64;sort_order=ascending;};{name=payload;type=string;}]",
                 ComplexYtPath.builder()
-                        .clusterName("hahn")
+                        .clusterName("test-cluster")
                         .basePath("//tmp/flink23-lookup")
                         .isPartitioned(false)
                         .enableDynamicStoreRead(true)
